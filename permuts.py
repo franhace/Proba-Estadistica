927481ced496a17de0246c63645f0b89691c82c7
Fachita <13649860+franhace@users.noreply.github.com>
2020-06-14 16:56:51 -0300

    ADD Jupyter notebooks de practica 5(TCL)

--- permuts.py
+++ permuts.py
@@ -44,6 +44,101 @@ vocales = len(set(perms_vocales))
 # print((consonantes-condiciones)*vocales)
 
 ######################## ######################## ######################## ########################
+#congruencia
+
+from itertools import permutations
+
+
+def lexicographical_permutation(str):
+    perm = sorted(''.join(chars) for chars in permutations(str))
+    for x in perm:
+        print(x)
+
+
+str = 'congruencia'
+# print(len(lexicographical_permutation(str)))
+
+# import library
+from math import factorial
+
+
+def lexicographical_permutations(str):
+    # there are going to be n ! permutations where n = len(seq)
+    for p in range(factorial(len(str))):
+        print(''.join(str))
+
+        i = len(str) - 1
+
+        # find i such that str[i:] is the largest sequence with
+        # elements in descending lexicographic order
+        while i > 0 and str[i - 1] > str[i]:
+            i -= 1
+
+        # reverse str[i:]
+        str[i:] = reversed(str[i:])
+
+        if i > 0:
+
+            q = i
+            # find q such that str[q] is the smallest element
+            # in str[p:] such that str[q] > str[i - 1]
+            while str[i - 1] > str[q]:
+                q += 1
+
+            # swap str[i - 1] and str[q]
+            temp = str[i - 1]
+            str[i - 1] = str[q]
+            str[q] = temp
+
+
+s = 'congruencia'
+s = list(s)
+s.sort()
+# lexicographical_permutations(s)
+
+
+# ii) forma de que vocales si o si entre n's
+def permuts(string):
+    permutaciones = [''.join(p) for p in permutations(string)]
+    not_nm_or_rtc = 0
+    for permutacion in permutaciones:
+        # posicion de cada letra
+        pos_n = permutacion.find('n')
+        pos_m = permutacion.find('m')
+        pos_r = permutacion.find('r')
+        pos_t = permutacion.find('t')
+        pos_c = permutacion.find('c')
+        # vemos si no cumple alguna de las condiciones
+        if not(pos_n < pos_m) or not(pos_r < pos_t < pos_c):
+            not_nm_or_rtc += 1
+        # Permutaciones que cumplen con ambas condiciones
+        # else:
+            # print(permutacion)
+    return not_nm_or_rtc
+#Consonantes
+perms_consonantes = [''.join(p) for p in permutations("cmbntrv")]
+# consonantes = len(perms_consonantes)
+
+#Condiciones
+# condiciones = permuts("cmbntrv")
+
+#Vocales
+perms_vocales = [''.join(p) for p in permutations('aaiioo')]
+# eliminamos los casos a1a2, a2a1, etc
+# vocales = len(set(perms_vocales))
+
+#(Conso - condiciones)*(Vocales)
+# print((consonantes-condiciones)*vocales)
+
+
+######################################################################
+"""
+Scriptcito para encontrar divisores enteros de un numero, 
+dadas ciertas condiciones para un n
+
+Ej: Hallar todos los n€N / 2n-2/n+4
+"""
+
 
 import math
 
@@ -57,7 +152,7 @@ def divisorGenerator(n):
     for divisor in reversed(large_divisors):
         yield int(divisor)
 
-lista_divisores_pos= list(divisorGenerator(16))
+lista_divisores_pos= list(divisorGenerator(9))
 
 def negativizar(lista_divisores_pos):
     lista_con_negs = []
@@ -75,7 +170,10 @@ def divisores_enteros(lista_divisores):
         # cambiar r por formula?? Prono a errores
         # r = 2, 3, 4
         # print(r[0])
-        r = (i+2)
+
+        # Escribir formula del n a despejar aca!
+        r = (i-1)/2
+
         # print(i, r)
         if float(r).is_integer():
             lista2.append(int(r))
@@ -83,8 +181,8 @@ def divisores_enteros(lista_divisores):
             lista2.append("X")
     return lista2
 
-# print(lista_con_todos_divisores)
-# print(divisores_enteros(lista_con_todos_divisores))
+print(lista_con_todos_divisores)
+print(divisores_enteros(lista_con_todos_divisores))
 
 ######################## ######################## ######################## ########################
 print(bin(1365))
@@ -104,7 +202,7 @@ def algo_div(numero):
         # print(result)
     print("The binary representation of %d is %s" %(numero_original, result))
 
-print(algo_div(133))
+# print(algo_div(133))
 
 
 def gcd(a, b):
@@ -117,9 +215,10 @@ def gcd(a, b):
 
     return a
 
-print(gcd(13,111))
+print(gcd(990,187))
 
 
+# Algo de la division de euclides
 def extendedEuclideanAlgorithm(a, b):
     if abs(b) > abs(a):
         (x, y, d) = extendedEuclideanAlgorithm(b, a)
@@ -136,6 +235,41 @@ def extendedEuclideanAlgorithm(a, b):
         a, b, x2, x1, y2, y1 = b, r, x1, x, y1, y
 
     return (x2, y2, a)
-print(extendedEuclideanAlgorithm(133, -14))
+print()
+print(extendedEuclideanAlgorithm(1038, 14))
 # (https://jeremykun.com/tag/division-algorithm/)
 ######################## ######################## ######################## ########################
+
+# suma de funciones enteras
+def ecuacion_a_cumplir1(comienzo, fin):
+    aes = []
+    for a in list(range(comienzo, fin)):
+        ecuacion = (a-2)/6
+        if ecuacion.is_integer():
+            aes.append(a)
+    print(aes)
+    return aes
+
+def ecuacion_a_cumplir2(comienzo, fin):
+    aes = []
+    for a in list(range(comienzo, fin)):
+        try:
+
+            ecuacion = (2*a - 1) / (a-1)
+            if a != 0 and ecuacion.is_integer():
+                aes.append(a)
+        except ZeroDivisionError:
+            pass
+
+    print(aes)
+    return aes
+
+# print(ecuacion_a_cumplir2())
+
+def todos_valores_que_cumples(comienzo, fin):
+    x1 = ecuacion_a_cumplir1(comienzo, fin)
+    x2 = ecuacion_a_cumplir2(comienzo, fin)
+    x = set(x1) & set(x2)
+    return x
+
+print(todos_valores_que_cumples(-200000,1000000))
