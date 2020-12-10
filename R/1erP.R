058272f76a9a2672cfdc7629d3e71b3af9f77343
Fachita <13649860+franhace@users.noreply.github.com>
2020-12-10 16:19:09 -0300

    ADD practicas en r

--- R/1erP.R
+++ R/1erP.R
@@ -1,5 +1,16 @@
 # Primer parcial
-a1<-sum(dexp(0:12,10))
-a2<-sum(dexp(0:12,12))
-pexp(1,5)
-1-a1*a2
+
+a<-exp(-12/13)
+a
+b<-exp(-12/10)
+b
+a*b
+pexp(10,12/13+12/10)
+1-pnorm(2)
+
+#pasa 7 veces A con ProbA = 0.4, 
+#pasa 2 veces B con ProbB = 0.35 y
+#pasa 3 veces C con ProbC = 0.25.
+dmultinom(x=c(1,13,18,8), prob = c(0.0228,0.3218,0.4967,0.1587))
+
+1-ppois(3,5)
